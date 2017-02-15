ec55e41bb2de218e0de543dd153951983f815c93
Frances <mynameislucah@yahoo.com>
2017-02-15 06:34:20 -0600

    checkpoint-24 commit

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ group :production do
    gem 'sqlite3'
  end
    gem 'bootstrap-sass'
+   gem 'bcrypt'
    gem 'sass-rails', '~> 5.0'
    gem 'uglifier', '>= 1.3.0'
    gem 'coffee-rails', '~> 4.1.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213132318) do
+ActiveRecord::Schema.define(version: 20170215122419) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20170213132318) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
