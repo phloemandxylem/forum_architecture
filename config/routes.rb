97731e3210ec4c04035da3e8af9d385888c1daa9
Frances <mynameislucah@yahoo.com>
2017-02-16 06:31:43 -0600

    commit checkpoint-26

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
             resources :posts, except: [:index]
       end
       resources :users, only: [:new, :create]
-  get 'about' => 'welcome#about'
-  root 'welcome#index'
+      resources :sessions, only: [:new, :create, :destroy]
+        get 'about' => 'welcome#about'
+        root 'welcome#index'
 end
