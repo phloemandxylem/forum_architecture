9df4c9905cca370459c8eed147a3e807a4af4f2e
Frances <mynameislucah@yahoo.com>
2017-02-20 06:42:37 -0600

    commit checkpoint-29

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe User, type: :model do
       it { is_expected.to validate_uniqueness_of(:email) }
       it { is_expected.to validate_length_of(:email).is_at_least(3) }
       it { is_expected.to allow_value("user@bloccit.com").for(:email) }
-
+      it { is_expected.to have_many(:comments) }
 
        it { is_expected.to validate_presence_of(:password) }
        it { is_expected.to have_secure_password }
